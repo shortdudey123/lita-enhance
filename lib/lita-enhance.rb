5d7b2dfb55ddc6e7754d4bebbb1e2eb064078923
Tristan Chong <tristanchong@gmail.com>
2015-08-10 14:38:38 -0700

    Add template_root filepath

--- lib/lita-enhance.rb
+++ lib/lita-enhance.rb
@@ -5,3 +5,8 @@ Lita.load_locales Dir[File.expand_path(
 )]
 
 require "lita/handlers/enhance"
+
+Lita::Handlers::Enhance.template_root File.expand_path(
+  File.join("..", "..", "templates"),
+ __FILE__
+)
